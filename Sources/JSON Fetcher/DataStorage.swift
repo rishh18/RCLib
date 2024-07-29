a2088e9a93f47cfd8cca84f2653a2a39b069d4cb
Rishabh Tripathi <Rishabh.tripathi@adpushup.com>
2024-07-29 22:21:59 +0530

    Initial Commit

---
+++ Sources/JSON Fetcher/DataStorage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rishabh Tripathi on 29/07/24.
+//
+
+import Foundation
---
+++ Tests/JSON FetcherTests/JSON_FetcherTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import JSON_Fetcher
+
+final class JSON_FetcherTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
